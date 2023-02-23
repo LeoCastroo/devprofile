9c5873ace517080d256e79fdf8ff637f50db20a6
LeoCastroo <leodcastro97@gmail.com>
2023-02-23 13:50:45 -0300

    feat: Adicionando o Hook form no projeto

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,91 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, KeyboardAvoidingView, Platform, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useForm, FieldValues } from 'react-hook-form';
 import { Button } from '../../components/Form/Button';
-import { Input } from '../../components/Form/Input';
-import { Container, Content, Logo, Title } from './styles';
+
+import {
+  Container,
+  Content,
+  CreateAccount,
+  CreateAccountTitle,
+  ForgotPasswordButton,
+  ForgotPasswordTitle,
+  Icon,
+  Logo,
+  Title,
+} from './styles';
 import logo from '../../assets/logo.png';
+import { InputControl } from '../../components/Form/InputControl';
+
+interface ScreenNavigationProp {
+  navigate: (screen: string) => void;
+}
+
+interface IFormInputs {
+  [name: string]: any;
+}
 
 export const SignIn: React.FunctionComponent = () => {
+  const { handleSubmit, control } = useForm<FieldValues>();
+  const { navigate } = useNavigation<ScreenNavigationProp>();
+
+  const handleSignIn = (form: IFormInputs) => {
+    const data = {
+      email: form.email,
+      password: form.password,
+    };
+
+    console.log(data);
+  };
+
   return (
-    <ScrollView
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={{ flex: 1 }}
+    <KeyboardAvoidingView
+      enabled
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
-      <Container>
-        <Content>
-          <Logo source={logo} />
-          <Title>Faça seu logon</Title>
-          <Input placeholder="Email" />
-          <Input placeholder="Senha" />
-          <Button title="Entrar" />
-        </Content>
-      </Container>
-    </ScrollView>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Content>
+            <Logo source={logo} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+            <InputControl
+              autoCapitalize="none"
+              autoCorrect={false}
+              control={control}
+              name="email"
+              placeholder="Email"
+              keyboardType="email-address"
+            />
+            <InputControl
+              control={control}
+              name="password"
+              placeholder="Senha"
+              autoCorrect={false}
+              secureTextEntry
+            />
+
+            <Button title="Entrar" onPress={handleSubmit(handleSignIn)} />
+            <ForgotPasswordButton>
+              <ForgotPasswordTitle>Esqueci minha senha</ForgotPasswordTitle>
+            </ForgotPasswordButton>
+          </Content>
+        </Container>
+      </ScrollView>
+      <CreateAccount
+        onPress={() => {
+          navigate('SignUp');
+        }}
+      >
+        <Icon name="log-in" />
+        <CreateAccountTitle>Criar uma conta</CreateAccountTitle>
+      </CreateAccount>
+    </KeyboardAvoidingView>
   );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Button } from '../../components/Form/Button';
 import { Input } from '../../components/Form/Input';
-import { Container, Content, Title } from './styles';
+import {
+  BackToSignIn,
+  BackToSignInTitle,
+  Container,
+  Content,
+  Icon,
+  Logo,
+  Title,
+} from './styles';
+import logo from '../../assets/logo.png';
+interface ScreenNavigationProp {
+  goBack: () => void;
+}
 
 export const SignUp: React.FunctionComponent = () => {
-  return (
-    <ScrollView
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={{ flex: 1 }}
-    >
-      <Container>
-        <Content>
-          <Title>Crie sua conta</Title>
-          <Input placeholder="Nome completo" />
-          <Input placeholder="Email" />
-          <Input placeholder="Senha" />
+  const { goBack } = useNavigation<ScreenNavigationProp>();
 
-          <Button title="Criar conta" />
-        </Content>
-      </Container>
-    </ScrollView>
+  return (
+    <KeyboardAvoidingView
+      enabled
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Content>
+            <Logo source={logo} />
+            <Title>Crie sua conta</Title>
+            <Input placeholder="Nome completo" />
+            <Input placeholder="Email" />
+            <Input placeholder="Senha" />
+
+            <Button title="Criar conta" />
+          </Content>
+        </Container>
+      </ScrollView>
+      <BackToSignIn onPress={() => goBack()}>
+        <Icon name="arrow-left" />
+        <BackToSignInTitle>Voltar para logon</BackToSignInTitle>
+      </BackToSignIn>
+    </KeyboardAvoidingView>
   );
 };
